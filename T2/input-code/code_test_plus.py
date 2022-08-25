66201b093fba003f28ba09d748f055a7239cc05c
alfre <alfredo.medina@uc.cl>
2022-08-25 15:51:55 -0400

    Done

---
+++ T2/input-code/code_test_plus.py
@@ -0,0 +1,9 @@
+def PlusPlus(x):
+    x = x + 5
+    return x
+
+class Person:
+    def PlusPlus(x):
+        x = x + 1
+        y += 1
+        return x
--- T2/transform.py
+++ T2/transform.py
@@ -3,6 +3,7 @@ import os
 from rewriter.rewriter import *
 from rewriter.eval_rewriter import *
 from rewriter.if_true_rewriter import *
+from rewriter.plusplus_rewriter import *
 
 path = "input-code/"
 dir_list = os.listdir(path)
@@ -10,15 +11,29 @@ dir_list = os.listdir(path)
 print("Transforming files in '", path, "' :")
 
 
-for file in dir_list:
-    print(" ==== " + file + " ==== ")
-    fileContent = open(path+file).read()
-    tree = parse(fileContent)
-    # we apply all rewriters in the file
-    for commandClass in RewriterCommand.__subclasses__():    
-        command = commandClass()
-        tree = command.apply(tree)
-    # export in a new file
-    f = open("transformed-code/"+file, "w")
-    f.write(unparse(tree))
-    f.close()
+# for file in dir_list:
+#     print(" ==== " + file + " ==== ")
+#     fileContent = open(path+file).read()
+#     tree = parse(fileContent)
+#     # we apply all rewriters in the file
+#     for commandClass in RewriterCommand.__subclasses__():    
+#         command = commandClass()
+#         tree = command.apply(tree)
+#     # export in a new file
+#     f = open("transformed-code/"+file, "w")
+#     f.write(unparse(tree))
+#     f.close()
+
+file = "code_test_plus.py"
+print(" ==== " + file + " ==== ")
+fileContent = open(path+file).read()
+tree = parse(fileContent)
+print(dump(tree, indent=2))  ## para imprimir el arbol
+# we apply all rewriters in the file
+for commandClass in RewriterCommand.__subclasses__():    
+    command = commandClass()
+    tree = command.apply(tree)
+# export in a new file
+f = open("transformed-code/"+file, "w")
+f.write(unparse(tree))
+f.close()
