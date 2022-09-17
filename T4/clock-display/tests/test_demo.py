3c018370308abe5a9a9e1cd86ae2b83d66854603
alfre <alfredo.medina@uc.cl>
2022-09-17 17:45:03 -0300

    more tests

--- T4/clock-display/tests/test_demo.py
+++ T4/clock-display/tests/test_demo.py
@@ -6,8 +6,21 @@ class TestDemo(TestCase):
 
     def setUp(self):
         self.factory = ClockFactory()
+        self.factory_2 = ClockFactory()
+        self.factory_3 = ClockFactory()
         self.clock = self.factory.create("hh:mm")
+        self.clock_2 = self.factory_2.create("hh:mm:ss")
+        self.clock_3 = self.factory_3.create("hh:mm:ss:mmmm")
 
+    def test_factory_1(self):
+        self.assertEqual(self.factory.create("hh:mm").str(), "00:00")
+    
+    def test_factory_2(self):
+        self.assertEqual(self.factory_2.create("hh:mm:ss").str(), "00:00:00")
+    
+    def test_factory_3(self):
+        self.assertEqual(self.factory_3.create("hh:mm:ss:mmmm").str(), "00:00:00:00")
+    
     def tearDown(self):
         pass
     
@@ -16,7 +29,11 @@ class TestDemo(TestCase):
     def test_demo(self):
         for i in range(10000):
             self.clock.increment()
-            print(self.clock.str())
+            # print(self.clock.str())
+
+    def test_str(self):
+        self.assertEqual(self.clock.str(), "00:00")
+        
 
     def test_invariant(self):
         for i in range(10000):
@@ -30,4 +47,25 @@ class TestDemo(TestCase):
         for i in self.clock.numbers:
             i.reset()
             self.assertEqual(i.value, 0)
-        
+
+    def test_clone(self):
+        for i in range(10000):
+            self.clock.increment()
+        clone = self.clock.clone()
+        self.assertEqual(self.clock.str(), clone.str())
+
+    def test_increment(self):
+        for i in range(24*60):
+            self.clock.increment()
+        self.assertEqual(self.clock.str(), "00:00")
+    
+    def test_increment_2(self):
+        for i in range(86400): # 86400 seconds = 24 hours
+            self.clock_2.increment()
+        self.assertEqual(self.clock_2.str(), "00:00:00")
+
+    def test_increment_3(self):
+        for i in range(10000): # 10000 milliseconds = 10 seconds
+            self.clock_3.increment()
+        self.assertEqual(self.clock_3.str(), "00:00:10:00")
+    
